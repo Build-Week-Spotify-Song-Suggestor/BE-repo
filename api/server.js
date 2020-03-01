8c54a3a1a122f5008e44a6ab31334007f3556596
ElkBranch <elggren.spencer@gmail.com>
2020-03-01 05:47:01 -0700

    Basis

---
+++ api/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+// server.use('/api/"data access point', authenticate, checkRole('user'), SongRouter);
+//Data Science will provide api access to song database
+//Talk with React to get what other endpoints they need
+
+module.exports = server;
+
+function checkRole(role) {
+    return (req, res, next) => {
+        if (
+            req.decodedToken &&
+            req.decodedToken.role &&
+            req.decodedToken.role.toLowerCase() === role
+        ) {
+            next();
+        } else {
+            res.status(403).json({ you: "shall not pass!" });
+        }
+    };
+}

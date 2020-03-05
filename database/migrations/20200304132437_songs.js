34827ac61c8c04065657cb722d1345a1400dcd11
ElkBranch <elggren.spencer@gmail.com>
2020-03-04 17:29:35 -0700

    song tables

---
+++ database/migrations/20200304132437_songs.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('songs', tbl => {
+      tbl.float('acousticness');
+      tbl.string('artist');
+      tbl.float('danceability');
+      tbl.float('duration_ms');
+      tbl.float('energy');
+      tbl.float('id');
+      tbl.float('instrumentalness');
+      tbl.float('key');
+      tbl.float('liveness');
+      tbl.float('loudness');
+      tbl.float('mode');
+      tbl.float('popularity');
+      tbl.float('speechiness');
+      tbl.float('tempo');
+      tbl.float('time_signature');
+      tbl.string('track_id').notNullable().unique();
+      tbl.string('track_name').notNullable().unique();
+      tbl.float('valence');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('songs');
+};
